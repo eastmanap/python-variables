b7ba687200f4f0921f63ce4f4c33e5ab17b24c6f
eastmanap <eastmanap354@northwested.org>
2024-09-06 10:38:02 -0400

    Update variables-practice.py

--- variables-practice.py
+++ variables-practice.py
@@ -1,5 +1,5 @@
-# Student name
-# 04 SEP 2024
+# apollos eastman
+# 06 SEP 2024
 # Variables Practice
 
 # Demonstrate in this script that you know how to:
@@ -15,18 +15,26 @@
 
 # Part 1 / Working with Text (String  Data)
 # Define a variable called school and assign it the string value Career Tech (using a pair of single quotes)
+school = 'career tech'
 # Define a variable called favorite_singer and assign it the string value that is your favorite singer (using a pair of double quotes)
+favorite_singer = "david bryne"
 # Define a variable called greeting and assign it a string value you'd use to greet someone (Good morning, Good afternoon, etc.)
+greeting = 'howdy do!'
 # Use the print function to print the value of one of three variables you created in Part 1
+print (greeting)
 
 
 
 
 # Part 2 / Working with Numbers (Numeric Data)
 # Define a variable called my_age and assign it your age as an integer
+my_age = '16 is my age'
 # Define a variable called conversion_factor and assign it the value of 2.54 (a float/floating-point number)
+conversion_factor = 2.54
 # Define a variable called book_price and assign it a value of 20.99 (a float/floating-point number)
+book_price = 20.99
 # Use the print function to print the value of one of three variables you created in Part 2
+print (my_age)
 
 
 
@@ -35,5 +43,15 @@
 # Create three different variables, one for each piece of data described below
 # Remember to follow the rules for naming Python variables
 # Use the print function to print your first name on your screen
+first_name = 'apollos'
+
+print (first_name)
 # Use the print function to print the name of the city you live in on your screen
+home_city = 'williamsburg'
+
+print (home_city)
 # Use the print function to print your age on your computer screen
+
+age = 16
+
+print (age)
